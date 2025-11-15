3d6d99a4f75ff49c648a9c00325df5e6d7cf697e
Kaleb Petersen <kcpetersen111@gmail.com>
2025-11-14 22:09:51 -0700

    build issue

--- backend/handlers/folder.go
+++ backend/handlers/folder.go
@@ -1,16 +1,10 @@
 package handlers
 
 import (
-<<<<<<< HEAD
 	"avenue/backend/persist"
 	"avenue/backend/shared"
-=======
->>>>>>> origin/main
 	"net/http"
 	"strconv"
-
-	"avenue/backend/persist"
-	"avenue/backend/shared"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tidwall/sjson"
